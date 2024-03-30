31d6bd88408693173928148d22d74741134c88eb
roeljohnbobis <roeljohn@users.noreply.github.com>
2024-03-30 09:20:43 +0800

    add dist and import in functions the js and css file

--- functions.php
+++ functions.php
@@ -3,6 +3,17 @@
  * The functions.php file behaves like a WordPress plugin, adding features and functionality to a 
  * WordPress site. You can use it to call WordPress functions and to define your own functions.
  */
+
+ 
+ 
+/**
+* Enqueue scripts and styles
+*/
+require_once( __DIR__ . '/functions/enqueue/import_css_js.php');
+
+/**
+* Add woocommerce support
+*/
 function customtheme_add_woocommerce_support(){
     add_theme_support( 'woocommerce' );
 }
